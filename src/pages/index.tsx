9d587fdb03a70e9c16d7ac7ebf029bf91f6ec856
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-11-05 23:32:10 -0300

    página inicial com estilo usando Chakra UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head'
+import { Flex, Text} from '@chakra-ui/react'
 
 export default function Home(){
   return(
     <>
       <Head>
-        <title>BarberPRO - Seu sistema completo</title>
+        <title>veterinário PRO - Seu sistema completo</title>
       </Head>
-      <div>
-        <h1>TESTE PROJETO</h1>
-      </div>
+      <Flex background="veterinario.900" height="100vh" alignItems="center" justifyContent="center">
+        <Text fontSize={30}>Página Home</Text>
+      </Flex>
     </>
   )
 }
