50371686f249ef0b63d6b1439ef86d15c11556ef
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-11-08 16:36:03 -0300

    função de login utilizando o contexto de autenticação no componente

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import Head from 'next/head'
 import Image from 'next/image';
 import logoImg from '../../../public/images/logo.svg'
@@ -6,14 +6,20 @@ import { Flex, Text, Center, Input, Button } from '@chakra-ui/react'
 
 import Link from 'next/link'
 
+import { AuthContext } from '../../context/AuthContext'
+
 export default function Login(){
+  const { signIn } = useContext(AuthContext)
+
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
 
 
-  function processarLogin(){
-    console.log(email);
-    console.log(senha);
+  async function processarLogin(){
+    await signIn({
+      email,
+      senha,
+    })
   }
 
   return(
