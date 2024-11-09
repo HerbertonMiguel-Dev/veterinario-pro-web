765a625252c75663703f84a30ce347f7de34d92e
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-11-09 00:04:19 -0300

    Drawer e MobileNav para navegação responsiva no Sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -14,17 +14,17 @@ import {
   FlexProps
 } from '@chakra-ui/react'
 
-import { FaRegCalendarAlt, FaStethoscope  } from 'react-icons/fa'
+import { FaRegCalendarAlt, FaStethoscope } from 'react-icons/fa'
 
 import {
   FiSettings,
   FiMenu
- } from 'react-icons/fi'
-import { IconType } from 'react-icons' 
+} from 'react-icons/fi'
+import { IconType } from 'react-icons'
 
 import Link from 'next/link'
 
-interface LinkItemProps{
+interface LinkItemProps {
   nome: string;
   icon: IconType;
   route: string;
@@ -36,29 +36,44 @@ const LinkItems: Array<LinkItemProps> = [
   { nome: 'Minha Conta', icon: FiSettings, route: '/profile' },
 ]
 
-export function Sidebar({ children }: { children: ReactNode }){
+export function Sidebar({ children }: { children: ReactNode }) {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return(
+  return (
     <Box minH="100vh" bg="veterinario.900">
       <SidebarContent
         onClose={() => onClose}
         display={{ base: 'none', md: 'block' }}
       />
 
-      <Box>
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+        onClose={onClose}
+      >
+        <DrawerContent>
+          <SidebarContent onClose={() => onClose()} />
+        </DrawerContent>
+      </Drawer>
+
+      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p={4}>
         {children}
       </Box>
     </Box>
   )
 }
 
-interface SidebarProps extends BoxProps{
+interface SidebarProps extends BoxProps {
   onClose: () => void;
 }
 
-const SidebarContent = ({onClose, ...rest}: SidebarProps) => {
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
   return (
     <Box
       bg="veterinario.400"
@@ -73,16 +88,16 @@ const SidebarContent = ({onClose, ...rest}: SidebarProps) => {
       <Flex h="20" alignItems="center" justifyContent="space-between" mx="8">
         <Link href="/dashboard">
           <Flex cursor="pointer" userSelect="none" flexDirection="row">
-            <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">veterinario</Text>
+            <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">Veterinário</Text>
             <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold" color="button.cta">PRO</Text>
           </Flex>
         </Link>
-        <CloseButton display={{ base: 'flex', md: 'none'}} onClick={onClose} />
+        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
 
       {LinkItems.map(link => (
         <NavItem icon={link.icon} route={link.route} key={link.nome}>
-           {link.nome} 
+          {link.nome}
         </NavItem>
       ))}
 
@@ -96,35 +111,71 @@ interface NavItemProps extends FlexProps {
   route: string;
 }
 
-const NavItem = ({icon, children, route, ...rest}: NavItemProps) => {
-  return(
-    <Link href={route} style={{ textDecoration: 'none'}} >
-    <Flex
-      align="center"
-      p="4"
-      mx="4"
-      borderRadius="lg"
-      role="group"
-      cursor="pointer"
-      _hover={{
-        bg: 'veterinario.900',
-        color: 'white'
-      }}
-      {...rest}
-    >
-     
-     {icon && (
-      <Icon
-        mr={4}
-        fontSize="16"
-        as={icon}
-        _groupHover={{
+const NavItem = ({ icon, children, route, ...rest }: NavItemProps) => {
+  return (
+    <Link href={route} style={{ textDecoration: 'none' }} >
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'veterinario.900',
           color: 'white'
         }}
+        {...rest}
+      >
+
+        {icon && (
+          <Icon
+            mr={4}
+            fontSize="16"
+            as={icon}
+            _groupHover={{
+              color: 'white'
+            }}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
+  )
+}
+
+interface MobileProps extends FlexProps{
+  onOpen: () => void;
+}
+
+const MobileNav = ({ onOpen, ...rest }: MobileProps ) => {
+  return(
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue('white', 'gray.900')}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent="flex-start"
+      {...rest}
+    >
+      <IconButton
+        variant="outline"
+        onClick={onOpen}
+        aria-label="open menu"
+        icon={ <FiMenu/> }
       />
-     )}
-     {children}
+  
+      <Flex flexDirection="row">
+        <Text ml={8} fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+          Veterinário
+        </Text>
+        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold" color="button.cta">
+          PRO
+        </Text>
+      </Flex>
     </Flex>
-  </Link>
   )
 }
